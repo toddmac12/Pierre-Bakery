55fa09e824e4d9a974dc1ecd0d53a478858747db
toddmac12 <toddmac12@gmail.com>
2021-09-24 16:23:46 -0400

    test passed

--- PierresBakery.Tests/ModelTests/BreadTests.cs
+++ PierresBakery.Tests/ModelTests/BreadTests.cs
@@ -11,8 +11,16 @@ namespace PierresBakery.Tests
     [TestMethod]
     public void BreadConstructor_CreatesInstanceOfBread_Bread()
     {
-      Bread newBread = new Bread();
+      Bread newBread = new Bread(5);
       Assert.AreEqual(typeof(Bread), newBread.GetType());
+    }
+ [TestMethod]
+    public void GetBreadPrice_ReturnsBreadPrice_Int()
+    {
+      int breadPrice = 5;
+      Bread newBread = new Bread(breadPrice);
+      int result = newBread.BreadPrice;
+      Assert.AreEqual(breadPrice, result);
     }
   }
 }
--- PierresBakery/Models/Bread.cs
+++ PierresBakery/Models/Bread.cs
@@ -7,5 +7,16 @@ namespace PierresBakery.Models
   {
       public int BreadPrice { get; set; } = 5;
       public int BreadQuantity {get; set;}
+   
+      public Bread(int breadQuantity)
+    {
+BreadQuantity = breadQuantity;
+    }
+    public int TotalBreadPrice()
+    {
+      int breadDeal = BreadPrice * ((BreadQuantity - (BreadQuantity % 3)) / 3);
+      int totalBreadPrice = (BreadQuantity * BreadPrice) - breadDeal;
+      return totalBreadPrice;
+    }
   }
 }
