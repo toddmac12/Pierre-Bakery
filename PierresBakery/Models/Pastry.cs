5246fb6b864e23a1367039c2a237bfec3984a540
toddmac12 <toddmac12@gmail.com>
2021-09-24 17:07:43 -0400

    test  pastry.cs test 3 passed

--- PierresBakery/Models/Pastry.cs
+++ PierresBakery/Models/Pastry.cs
@@ -13,7 +13,23 @@ namespace PierresBakery.Models
     {
     PastryQuantity = pastryQuantity;
     }
-
-
+ public int TotalPastryPrice()
+    {
+      if (PastryQuantity % 3 == 0)
+      {
+        int totalPastryPrice = (PastryQuantity / 3) * 5;
+        return totalPastryPrice;
+      }
+      else if (PastryQuantity % 3 > 0 && PastryQuantity > 3)
+      {
+        int totalPastryPrice = ((PastryQuantity / 3) * 5) + ((PastryQuantity % 3) * PastryPrice);
+        return totalPastryPrice;
+      }
+      else
+      {
+        return PastryPrice * PastryQuantity;
+      }
+      
+    }
   }
 }
